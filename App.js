2fcd83883f762b392cdc1e8d3a5b7599505e0022
ecruz17 <ecruz17@ucol.mx>
2022-11-03 18:45:52 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Switch, Text, TextInput, View, Alert } from 'react-native';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component<Props>{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+      inputText: 'Write here'
+    }
+  }
+
+  onPressBtn = () => {
+    console.log('jijija');
+  }
+
+  onChange = (value) => {
+    console.log(value);
+    this.setState({ switchValue: value });
+  }
+
+  onTextInputChange = (input) => {
+    this.setState({
+      inputText: input
+    });
+  }
+
+  createTwoButtonAlert = () => {
+    Alert.alert(
+      "Alert Title",
+      this.state.inputText,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.inputText}
+          onChangeText={this.onTextInputChange}
+        />
+        <Text>Hola bandiuks</Text>
+        <Button
+          onPress={() => { this.onPressBtn(); this.createTwoButtonAlert() }}
+          title='Picalee'
+        />
+        <Switch
+          value={this.state.switchValue} //valor dinámico para que se renderize diferente el switch
+          onValueChange={() => this.onChange(!this.state.switchValue)}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
